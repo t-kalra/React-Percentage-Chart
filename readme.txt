904b33ed86e3e68741c77d0e73a138691d1d44ff
Tarun Kalra <34830546+t-kalra@users.noreply.github.com>
2017-12-26 11:14:30 +0530

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,9 @@
+ReactJS Percentage Chart
+
+Step to run
+1. npm install - to install the node packages
+2. npm run build - to build the bundle.js
+3. npm run start - to start the server 
+
+To access the app
+http://localhost:8080/chart/
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  devtool: process.env.NODE_ENV === 'production' ? false : 'eval-source-map',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    //.path.resolve(__dirname, 'src/index')
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'chart'),
+    publicPath: '/chart/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+    //new webpack.NoErrorsPlugin()
+    //extractSass
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;
